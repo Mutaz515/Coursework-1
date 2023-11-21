d0941301b66b5e4a8af519686c81ac0a4f4eeeb5
Mutaz Al Mahamid <almahamidsweden@gmail.com>
2023-11-21 16:58:09 +0400

    add to cart added

--- app.js
+++ app.js
@@ -6,9 +6,26 @@ var webstore = new Vue({
         cart: [],
     },
     methods: {
-
+        addToCart: function (lesson) {
+            this.cart.push( lesson.id);
+        },
+        canAddToCart: function(lesson) {
+            return lesson.spaces > this.cartCount(lesson.id);
+        },
+        cartCount(id) {
+            let count = 0;
+            for (let i = 0; i < this.cart.length; i++) {
+                if (this.cart[i] === id) {
+                    count++;
+                }
+            }
+            return count;
+        }
     },
     computed: {
-        
+        cartItemCount: function() {
+            return this.cart.length ;
+        },
+       
     }
 });
